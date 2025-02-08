b4540b2c15627504e5315f298467b82e63c6bdb9
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2025-02-08 15:13:21 -0500

    v1.6 - Naming Changes
    Changed "Card Selection" to "Piece Selection". Added mouse-hover and mouse-click block fill color change code to Piece Placement module

--- cardSelection.js
+++ cardSelection.js
@@ -1,74 +1,74 @@
 //initializing base variables
-let CS_totalPlayers = 2;
-let CS_playerNames = [];
-let CS_totalCards = 4;
+let PS_totalPlayers = 2;
+let PS_playerNames = [];
+let PS_totalCards = 5;
 
 //initializing card placeholder position variables
-let CS_cards_initxPos;
-let CS_cards_separation;
-let CS_cards_size;
+let PS_cards_initxPos;
+let PS_cards_separation;
+let PS_cards_size;
 
 //initializing aesthetic variables - red color, fonts, card & background images
-let CS_backgroundImage;
-let CS_redShade = "#FF0000";
-let CS_fontHeading, CS_fontBody, CS_fontAccent;
-let CS_P1_cardImage_Q, CS_P1_cardImage_B, CS_P1_cardImage_N, CS_P1_cardImage_R, CS_P1_cardImage_P;
-let CS_P2_cardImage_Q, CS_P2_cardImage_B, CS_P2_cardImage_N, CS_P2_cardImage_R, CS_P2_cardImage_P;
+let PS_backgroundImage;
+let PS_redShade = "#FF0000";
+let PS_fontHeading, PS_fontBody, PS_fontAccent;
+let PS_P1_cardImage_Q, PS_P1_cardImage_B, PS_P1_cardImage_N, PS_P1_cardImage_R, PS_P1_cardImage_P;
+let PS_P2_cardImage_Q, PS_P2_cardImage_B, PS_P2_cardImage_N, PS_P2_cardImage_R, PS_P2_cardImage_P;
 
 //initializing input field variables
-let CS_nameField, CS_cardLetterField;
+let PS_nameField, PS_cardLetterField;
 
 //initializing card counter and empty arrays for storing players' card values
-let CS_playerCount = 1;
-let CS_cardCount = 0;
-let CS_P1_Cards = [];
-let CS_P2_Cards = [];
+let PS_playerCount = 1;
+let PS_cardCount = 1;
+let PS_P1_Cards = ['K'];
+let PS_P2_Cards = ['K'];
 
 function preload()
 {
-    CS_backgroundImage = loadImage("./Assets/BG_Image.png");
+    PS_backgroundImage = loadImage("./Assets/BG_Image.png");
 
     //pre-loading typefaces
-    CS_fontHeading = loadFont("./Assets/BlackOpsOne.ttf");
-    CS_fontAccent = loadFont("./Assets/LexendTera.ttf");
-    CS_fontBody = loadFont("./Assets/NATS.ttf");
+    PS_fontHeading = loadFont("./Assets/BlackOpsOne.ttf");
+    PS_fontAccent = loadFont("./Assets/LexendTera.ttf");
+    PS_fontBody = loadFont("./Assets/NATS.ttf");
 
     //pre-loading player 1 card images
-    CS_P1_cardImage_Q = loadImage("./Assets/Player1_Queen.png");
-    CS_P1_cardImage_B = loadImage("./Assets/Player1_Bishop.png");
-    CS_P1_cardImage_N = loadImage("./Assets/Player1_Knight.png");
-    CS_P1_cardImage_R = loadImage("./Assets/Player1_Rook.png");
-    CS_P1_cardImage_P = loadImage("./Assets/Player1_Pawn.png");
+    PS_P1_cardImage_Q = loadImage("./Assets/Player1_Queen.png");
+    PS_P1_cardImage_B = loadImage("./Assets/Player1_Bishop.png");
+    PS_P1_cardImage_N = loadImage("./Assets/Player1_Knight.png");
+    PS_P1_cardImage_R = loadImage("./Assets/Player1_Rook.png");
+    PS_P1_cardImage_P = loadImage("./Assets/Player1_Pawn.png");
 
     //pre-loading player 2 card images
-    CS_P2_cardImage_Q = loadImage("./Assets/Player2_Queen.png");
-    CS_P2_cardImage_B = loadImage("./Assets/Player2_Bishop.png");
-    CS_P2_cardImage_N = loadImage("./Assets/Player2_Knight.png");
-    CS_P2_cardImage_R = loadImage("./Assets/Player2_Rook.png");
-    CS_P2_cardImage_P = loadImage("./Assets/Player2_Pawn.png");
+    PS_P2_cardImage_Q = loadImage("./Assets/Player2_Queen.png");
+    PS_P2_cardImage_B = loadImage("./Assets/Player2_Bishop.png");
+    PS_P2_cardImage_N = loadImage("./Assets/Player2_Knight.png");
+    PS_P2_cardImage_R = loadImage("./Assets/Player2_Rook.png");
+    PS_P2_cardImage_P = loadImage("./Assets/Player2_Pawn.png");
 }
 
 function setup()
 {
     createCanvas(windowWidth, windowHeight);
-    background(CS_backgroundImage);
+    background(PS_backgroundImage);
 
     //creating name input field
-    CS_nameField = createInput();
-    CS_nameField.size(300, 30);
-    CS_nameField.position(width/2.5, height/2);
+    PS_nameField = createInput();
+    PS_nameField.size(300, 30);
+    PS_nameField.position(width/2.5, height/2);
 
     //creating card letter input field
-    CS_cardLetterField = createInput();
-    CS_cardLetterField.hide();
-    CS_cardLetterField.size(30, 30);
-    CS_cardLetterField.position(width/1.92, height-(height/8.5));
+    PS_cardLetterField = createInput();
+    PS_cardLetterField.hide();
+    PS_cardLetterField.size(30, 30);
+    PS_cardLetterField.position(width/1.9, height-(height/8.5));
 
     //setting card height to scale according to screen-height
-    CS_cards_size = height/3;
-    CS_cards_separation = 325;
+    PS_cards_size = height/3;
+    PS_cards_separation = 325;
 
-    //generating chessboard for piece placement segment
+    //generating chessboard for piece placement segment -- runs in piecePlacement.js file
     let PP_init_block_yPos = int(height/3.75);
     for(let PP_blockRow=0; PP_blockRow<PP_chessboardSize; PP_blockRow++)
     {
@@ -87,140 +87,140 @@ function setup()
 
 function draw()
 {
-    if(CS_playerCount <= CS_totalPlayers)
+    if(PS_playerCount <= PS_totalPlayers)
     {
         //function to display on-screen text
-        CS_playerTextDisplay();
+        PS_playerTextDisplay();
 
         //condition to check player name entry
-        if(keyCode == ENTER && CS_nameField.value() != "")
+        if(keyCode == ENTER && PS_nameField.value() != "")
         {
             //name value pushed to player name array on pressing enter key
-            CS_playerNames.push(CS_nameField.value());
-            CS_nameField.value("");
-            CS_nameField.hide();
+            PS_playerNames.push(PS_nameField.value());
+            PS_nameField.value("");
+            PS_nameField.hide();
 
             //generating card placeholder shapes
-            background(CS_backgroundImage);
+            background(PS_backgroundImage);
             rectMode(CENTER);
             fill(255);
             noStroke();
 
-            CS_cards_initxPos = width/5.5;
-            for(let i=0; i<CS_totalCards; i++)
+            PS_cards_initxPos = width/5.5;
+            for(let i=1; i<PS_totalCards; i++)
             {
-                rect(CS_cards_initxPos, height/1.8, CS_cards_size, CS_cards_size/0.64, 15);
-                CS_cards_initxPos += CS_cards_separation;
+                rect(PS_cards_initxPos, height/1.8, PS_cards_size, PS_cards_size/0.64, 15);
+                PS_cards_initxPos += PS_cards_separation;
             }
 
             //resetting cards' initial position for superimposing actual card images on card selection
-            CS_cards_initxPos = width/5.5;
+            PS_cards_initxPos = width/5.5;
         }
 
         //condition to check card values entered in input field
-        if(keyCode == ENTER && CS_cardCount < CS_totalCards && (CS_cardLetterField.value() == 'Q' || CS_cardLetterField.value() == 'B' || CS_cardLetterField.value() == 'N' || CS_cardLetterField.value() == 'R' || CS_cardLetterField.value() == 'P'))
+        if(keyCode == ENTER && PS_cardCount < PS_totalCards && (PS_cardLetterField.value() == 'Q' || PS_cardLetterField.value() == 'B' || PS_cardLetterField.value() == 'N' || PS_cardLetterField.value() == 'R' || PS_cardLetterField.value() == 'P'))
         {
-            if(CS_playerCount == 1)
+            if(PS_playerCount == 1)
             {
                 //pushing letter values onto players' card value array, and resetting input field to blank
-                CS_P1_Cards.push(CS_cardLetterField.value());
-                CS_cardLetterField.value("");
+                PS_P1_Cards.push(PS_cardLetterField.value());
+                PS_cardLetterField.value("");
 
                 //generating actual card images for the values entered
                 //function decides which image to generate depending on value passed from letter input field
                 imageMode(CENTER);
-                image(CS_showCardImage(CS_P1_Cards[CS_cardCount]), CS_cards_initxPos, height/1.8, CS_cards_size, CS_cards_size/0.64);
-                CS_cards_initxPos += CS_cards_separation;
+                image(PS_showCardImage(PS_P1_Cards[PS_cardCount]), PS_cards_initxPos, height/1.8, PS_cards_size, PS_cards_size/0.64);
+                PS_cards_initxPos += PS_cards_separation;
             }
             //same thing as above, but for player 2
-            else if(CS_playerCount == 2)
+            else if(PS_playerCount == 2)
             {
-                CS_P2_Cards.push(CS_cardLetterField.value());
-                CS_cardLetterField.value("");
+                PS_P2_Cards.push(PS_cardLetterField.value());
+                PS_cardLetterField.value("");
 
                 imageMode(CENTER);
-                image(CS_showCardImage(CS_P2_Cards[CS_cardCount]), CS_cards_initxPos, height/1.8, CS_cards_size, CS_cards_size/0.64);
-                CS_cards_initxPos += CS_cards_separation;
+                image(PS_showCardImage(PS_P2_Cards[PS_cardCount]), PS_cards_initxPos, height/1.8, PS_cards_size, PS_cards_size/0.64);
+                PS_cards_initxPos += PS_cards_separation;
             }
             //counter to check total cards picked by each player
-            CS_cardCount++;
+            PS_cardCount++;
         }
         //once all cards picked, increment player count variable and restart process for next player
-        else if(CS_cardCount >= CS_totalCards && CS_playerCount <= CS_totalPlayers)
+        else if(PS_cardCount >= PS_totalCards && PS_playerCount <= PS_totalPlayers)
         {
             //hide the input field box after all cards picked
-            CS_cardLetterField.hide();
+            PS_cardLetterField.hide();
 
             //continue to another by pressing spacebar
             if(keyCode == 32)
             {
                 //reset card counter for next player & bring up input field for entering player name
-                CS_playerCount++;
-                CS_cardCount = 0;
-                CS_nameField.show();
+                PS_playerCount++;
+                PS_cardCount = 1;
+                PS_nameField.show();
 
                 //resetting image mode so that the background comes fullscreen
                 imageMode(CORNER);
-                background(CS_backgroundImage);
+                background(PS_backgroundImage);
             }
         }
     }
     //if all players done with their card selections, move onto piece placement file
     else
     {
-        CS_nameField.hide();
+        PS_nameField.hide();
 
-        //function to proceed to piece placement step
+        // function to proceed to piece placement step
         piecePlacement();
     }
 }
 
 //function definition for displaying on-screen text
-function CS_playerTextDisplay()
+function PS_playerTextDisplay()
 {
     textAlign(CENTER,CENTER);
-    textFont(CS_fontAccent);
+    textFont(PS_fontAccent);
     textSize(32);
-    fill(CS_redShade);
+    fill(PS_redShade);
     noStroke();
-    text("CARD SELECTION", width/2, height/12);
+    text("PIECE SELECTION", width/2, height/12);
 
-    textFont(CS_fontHeading);
+    textFont(PS_fontHeading);
     fill(255);
     
     //code to display text before player has entered name
-    if(CS_playerNames[CS_playerCount-1] == undefined)
+    if(PS_playerNames[PS_playerCount-1] == undefined)
     {
-        text("PLAYER "+CS_playerCount, width/2, height/7.5);
+        text("PLAYER "+PS_playerCount, width/2, height/7.5);
         
-        textFont(CS_fontBody);
+        textFont(PS_fontBody);
         textSize(30);
-        text("Enter Player "+CS_playerCount+"'s Name", width/2, height/2.2);
+        text("Enter Player "+PS_playerCount+"'s Name", width/2, height/2.2);
     }
     //code to display text after player has entered name
     else
     {
-        //code to display text for the card picking process
-        if(CS_cardCount < CS_totalCards)
+        //code to display text for the piece picking process
+        if(PS_cardCount < PS_totalCards)
         {
-            text("PLAYER "+CS_playerCount+": "+CS_playerNames[CS_playerCount-1], width/2, height/7.5);
+            text("PLAYER "+PS_playerCount+": "+PS_playerNames[PS_playerCount-1], width/2, height/7.5);
 
-            textFont(CS_fontBody);
+            textFont(PS_fontBody);
             textSize(30);
-            text("Cards Picked From Deck", width/2, height/4.5);
-            text("Card Letter: ", width/2.1, height-(height/9));
-            CS_cardLetterField.show();
+            text("Pieces Picked From Deck", width/2, height/4.5);
+            text("Piece Letter: ", width/2.1, height-(height/9));
+            PS_cardLetterField.show();
         }
-        //code to hide text after all cards picked for a player
+        //code to hide text after all pieces picked for a player
         else
         {
             rectMode(CORNER);
-            fill(CS_redShade);
+            fill(PS_redShade);
             noStroke();
             rect(0, height-(height/9), width, height);
             
             fill(255);
-            textFont(CS_fontBody);
+            textFont(PS_fontBody);
             textSize(25);
             text("Done! Press [SPACE] to continue.", width/2, height-(height/15));
         }
@@ -228,37 +228,37 @@ function CS_playerTextDisplay()
 }
 
 //function to return variable name of actual image card depending on letter value received from input field
-function CS_showCardImage(letterValue)
+function PS_showCardImage(letterValue)
 {
-    if(CS_playerCount == 1)
+    if(PS_playerCount == 1)
     {
         switch(letterValue)
         {
-            case 'Q': return CS_P1_cardImage_Q;
+            case 'Q': return PS_P1_cardImage_Q;
             break;
-            case 'B': return CS_P1_cardImage_B;
+            case 'B': return PS_P1_cardImage_B;
             break;
-            case 'N': return CS_P1_cardImage_N;
+            case 'N': return PS_P1_cardImage_N;
             break;
-            case 'R': return CS_P1_cardImage_R;
+            case 'R': return PS_P1_cardImage_R;
             break;
-            case 'P': return CS_P1_cardImage_P;
+            case 'P': return PS_P1_cardImage_P;
             break;
         }
     }
-    else if(CS_playerCount == 2)
+    else if(PS_playerCount == 2)
     {
         switch(letterValue)
         {
-            case 'Q': return CS_P2_cardImage_Q;
+            case 'Q': return PS_P2_cardImage_Q;
             break;
-            case 'B': return CS_P2_cardImage_B;
+            case 'B': return PS_P2_cardImage_B;
             break;
-            case 'N': return CS_P2_cardImage_N;
+            case 'N': return PS_P2_cardImage_N;
             break;
-            case 'R': return CS_P2_cardImage_R;
+            case 'R': return PS_P2_cardImage_R;
             break;
-            case 'P': return CS_P2_cardImage_P;
+            case 'P': return PS_P2_cardImage_P;
             break;
         }
     }
--- piecePlacement.js
+++ piecePlacement.js
@@ -1,10 +1,14 @@
+//initializing all Piece Placement segment variables
 let PP_playerCount = 1;
 let PP_chessboardSize = 8;
 let PP_blocksArray = [];
 
+let PP_hoverShade = "#00FF00";
+
 //class declaration for the chessboard
 class PP_Chessboard
 {
+    //constructor function for initializing all chessboard-related variables
     constructor(_PP_block_xPos, _PP_block_yPos, _PP_block_idRow, _PP_block_idCol)
     {
         this.PP_block_xPos = _PP_block_xPos;
@@ -14,31 +18,68 @@ class PP_Chessboard
         this.PP_block_idRow = _PP_block_idRow;
         this.PP_block_idCol = _PP_block_idCol;
         this.PP_block_fillColor = color(255);
-        this.PP_block_strokeColor = color(0);
+        this.PP_block_strokeColor = color(255);
+        
+        this.PP_block_hoverFlag = 0;
+        this.PP_block_occupiedFlag = 0;
+        this.PP_block_playerNumber = 0;
     }
+    //function to generate individual blocks on-screen
     PP_drawBlock()
     {
         rectMode(CENTER);        
         stroke(this.PP_block_strokeColor);
         noStroke();
 
-        if(this.PP_block_idRow%2==0)
+        // conditional statements to alternate b/w white & red shades for the blocks
+        // also to change fill color dynamically on mouse-hover
+        if(this.PP_block_idRow%2 == 0)
         {
-            if(this.PP_block_idCol%2!=0)
+            if(this.PP_block_idCol%2 != 0 && this.PP_block_hoverFlag == 0)
             {
-                this.PP_block_fillColor = color(255,0,0);
+                this.PP_block_fillColor = PS_redShade;
             }
         }
         else
         {
-            if(this.PP_block_idCol%2==0)
+            if(this.PP_block_idCol%2 == 0 && this.PP_block_hoverFlag == 0)
             {
-                this.PP_block_fillColor = color(255,0,0);
+                this.PP_block_fillColor = PS_redShade;
             }
+        }
+
+        //change block fill color to black once block becomes occupied
+        if(this.PP_block_occupiedFlag == 1)
+        {
+            this.PP_block_fillColor = color(0);
         }
 
         fill(this.PP_block_fillColor);
         rect(this.PP_block_xPos, this.PP_block_yPos, this.PP_block_size);
+    }
+    //function to determine when mousepointer is hovering over a block & toggle hoverflag value accordingly
+    PP_hoverBlock()
+    {
+        if(mouseX <= this.PP_block_xPos+this.PP_block_size/2 && mouseX >= this.PP_block_xPos-this.PP_block_size/2 && mouseY <= this.PP_block_yPos+this.PP_block_size/2 && mouseY >= this.PP_block_yPos-this.PP_block_size/2)
+        {
+            this.PP_block_hoverFlag = 1;
+            this.PP_block_fillColor = PP_hoverShade;
+        }
+        else
+        {
+            this.PP_block_hoverFlag = 0;
+            this.PP_block_fillColor = color(255);
+        }
+    }    
+}
+
+//class declaration for player 1 pieces
+class PP_P1_Pieces
+{
+    constructor(_PP_pieceType)
+    {
+        this.PP_pieceType = _PP_pieceType;
+        this.PP_piecePosition = [];
     }
 }
 
@@ -47,11 +88,31 @@ function piecePlacement()
 {
     PP_playerTextDisplay();
 
+    //generating chessboard and running all functions
     for(let PP_blockRow=0; PP_blockRow<PP_chessboardSize; PP_blockRow++)
     {
         for(let PP_blockCol=0; PP_blockCol<PP_chessboardSize; PP_blockCol++)
         {
             PP_blocksArray[PP_blockRow][PP_blockCol].PP_drawBlock();
+
+            if(PP_playerCount == 1)
+            {
+                //running loop for player 1 - piece placement on left-most 3 columns
+                for(let i=0; i<3; i++)
+                {
+                    PP_blocksArray[PP_blockRow][i].PP_hoverBlock();
+                    if(mouseButton == LEFT && mouseIsPressed == true && PP_blocksArray[PP_blockRow][i].PP_block_hoverFlag == 1 && PP_blocksArray[PP_blockRow][i].PP_block_occupiedFlag != 1)
+                    {
+                        PP_blocksArray[PP_blockRow][i].PP_block_occupiedFlag = 1;
+                        mouseIsPressed = false;
+                    }
+                }
+            }
+            else if(PP_playerCount == 2)
+            {
+                //running loop for player 2 - piece placement on right-most 3 columns
+                //COPY CODE HERE WHEN COMPLETE WITH PLAYER 1
+            }
         }
     }
 }
@@ -60,13 +121,13 @@ function piecePlacement()
 function PP_playerTextDisplay()
 {
     textAlign(CENTER,CENTER);
-    textFont(CS_fontAccent);
+    textFont(PS_fontAccent);
     textSize(32);
-    fill(CS_redShade);
+    fill(PS_redShade);
     noStroke();
     text("PIECE PLACEMENT", width/2, height/12);
 
-    textFont(CS_fontHeading);
+    textFont(PS_fontHeading);
     fill(255);
-    text("PLAYER "+PP_playerCount+": "+CS_playerNames[PP_playerCount-1], width/2, height/7.5);
+    text("PLAYER "+PP_playerCount+": "+PS_playerNames[PP_playerCount-1], width/2, height/7.5);
 }
