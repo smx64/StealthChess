5e599d367595536b831214e3f23ef0d60a8c7e2a
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2025-03-19 22:29:26 -0400

    v9.4.3.1

--- cardSelection.js
+++ cardSelection.js
@@ -70,7 +70,7 @@ function preload()
     //pre-loading background score & sound effects - used in piecePlacement.js & gamePlay.js
     PP_bgm = loadSound("./Assets/Music_PiecePlacement.mp3");
     GP_bgm = loadSound("./Assets/Music_Gameplay.mp3");
-    PP_SFX_pieceClicked = loadSound("/Assets/SFX_pieceClicked.mp3");
+    PP_SFX_pieceClicked = loadSound("./Assets/SFX_pieceClicked.mp3");
     // PP_SFX_pieceMoved = loadSound("Assets/SFX_PieceMoved.mp3");
     // GP_SFX_pieceChecked = loadSound("./Assets/SFX_pieceChecked.mp3");
 }
